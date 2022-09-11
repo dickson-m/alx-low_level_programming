520a555560de1a114e1d2e80988bc4e35bff6b5e
Dickson Makulia <makulia.dickson@gmail.com>
2022-09-11 02:58:28 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+/**
+ *
+ *main - my project entrypoint
+ *
+ * Return: always 0 in here
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - project entrypoint
+ *
+ * Return: returns 0
+ *
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long lo;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long: %lu byte(s)\n", sizeof(lo));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	return (0);
+}
